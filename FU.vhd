749f2c7eb74d17470a72c0c5c3406b6c7f075247
Ahmed Emad <74673544+ahmedemad81@users.noreply.github.com>
2023-04-19 03:50:54 +0200

    Wrap Execution Stage

--- FU.vhd
+++ FU.vhd
@@ -1,25 +1,17 @@
--- LIBRARY IEEE;
--- USE IEEE.std_logic_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
 
 
--- -- not done phase 2
--- ENTITY FU IS
---     PORT (
---         dataIn : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
---         clk, rst, enable : IN STD_LOGIC;
---         dataOut : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
--- END ENTITY FU;
+--not done phase 2
+ENTITY FU IS
+    PORT (
+        dataIn : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        clk, rst, enable : IN STD_LOGIC;
+        dataOut : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
+END ENTITY FU;
 
--- ARCHITECTURE fuArch OF FU IS
--- BEGIN
+ARCHITECTURE fuArch OF FU IS
+BEGIN
 
---     PROCESS (clk, rst)
---     BEGIN
---         IF (rst = '1') THEN
---             dataOut <= (others => '0');
---         ELSIF clk'event AND clk = '1' AND enable = '1' THEN
---             dataOut <= dataIn;
---         END IF;
---     END PROCESS;
--- END fuArch;
+END fuArch;
 
---
+++ executionStage.vhd
@@ -0,0 +1,41 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.numeric_std.ALL;
+
+ENTITY executionStage IS
+    PORT (
+        Op1,Op2,inPort : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        EM_OP,MM_OP,MWB_OP,immediateOP: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        S1_FU,S2_FU : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+        isImmediate : IN STD_LOGIC;
+        inPortEnable : IN STD_LOGIC;
+        aluOp : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        aluOut : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        carryOutFlag,zeroFlag,negativeFlag : OUT STD_LOGIC        
+    );
+END ENTITY executionStage;
+
+
+ARCHITECTURE executionStageArch OF executionStage IS
+    signal firstOperand,secondOperand,Op2Temp,aluOutTemp : STD_LOGIC_VECTOR(15 DOWNTO 0);
+BEGIN
+
+    ALU : entity work.alu port map(firstOperand,secondOperand,aluOp,aluOutTemp,carryOutFlag,zeroFlag,negativeFlag);
+    --Selecting the first operand
+    firstOperand <= Op1 when S1_FU = "00" else
+                    EM_OP when S1_FU = "01" else
+                    MM_OP when S1_FU = "10" else
+                    MWB_OP when S1_FU = "11";
+    --Selecting if the second operand is immediate or not
+    Op2Temp <= Op2 when isImmediate = '0' else
+                immediateOP;
+    --Selecting the second operand
+    secondOperand <= Op2Temp when S2_FU = "00" else
+                    EM_OP when S2_FU = "01" else
+                    MM_OP when S2_FU = "10" else
+                    MWB_OP when S2_FU = "11";  
+    --Outputing the alu output
+    aluOut <= aluOutTemp when inPortEnable = '0' else
+                    inPort;
+
+END executionStageArch;
