38d5925df0d9014024da280f0d769160f5458359
Mostafa Osama <31553087+Battarshuck@users.noreply.github.com>
2023-04-19 19:52:12 +0200

    done memory stage and modified data memory

--- memory.vhd
+++ memory.vhd
@@ -3,10 +3,10 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.numeric_std.all;
 ENTITY memory IS
 PORT (
-    WriteData: in std_logic_vector(15 downto 0);--data in
-    ReadData: out std_logic_vector(15 downto 0);--data out
-    MemWrite, MemRead, reset, clk : IN std_logic;
-    Address: in std_logic_vector(9 downto 0)
+    clk, reset, MemWrite, MemRead : IN std_logic;
+    WriteData: in std_logic_vector(15 downto 0);--data in  
+    Address: in std_logic_vector(15 downto 0); --use the first 10 bits only
+    ReadData: out std_logic_vector(15 downto 0)--data out 
 );
 END ENTITY memory;
 
@@ -21,14 +21,12 @@ BEGIN
     for u in ram' range loop                 
         ram(u) := (others=>'0');
     end loop;
+    elsif rising_edge(clk) and MemWrite = '1' THEN	
+        ram(to_integer(unsigned((Address(9 downto 0))))) := WriteData;
+    END IF;
 
-    elsif rising_edge(clk) THEN
-    	IF MemWrite = '1' THEN
-        	ram(to_integer(unsigned((Address)))) := WriteData;
-    	END IF;
-		IF MemRead = '1' THEN
-			ReadData <= ram(to_integer(unsigned((Address))));
-		END IF;
+    IF falling_edge(clk) and MemRead = '1' then
+        ReadData <= ram(to_integer(unsigned((Address(9 downto 0)))));
     END IF;
 END PROCESS;
     --Q <= ram(to_integer(unsigned((readAddr))));
