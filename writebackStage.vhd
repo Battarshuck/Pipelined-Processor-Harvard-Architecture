bff8963985e8dc8afa1be2f49dd8c02e71d05138
Mostafa Osama <31553087+Battarshuck@users.noreply.github.com>
2023-04-19 17:48:33 +0200

    writeback stage done

---
+++ writebackStage.vhd
@@ -0,0 +1,21 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.numeric_std.ALL;
+
+ENTITY writebackStage IS
+    PORT (
+        memToRegControl: IN STD_LOGIC;
+        dataFromMemory, dataFromALU : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        dataOut : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+        ); 
+END ENTITY writebackStage;
+
+
+ARCHITECTURE writebackStageArch OF writebackStage IS   
+BEGIN
+    
+WITH memToRegControl SELECT
+    dataOut <= dataFromALU WHEN '0',
+            dataFromMemory WHEN OTHERS;
+    
+END writebackStageArch;
