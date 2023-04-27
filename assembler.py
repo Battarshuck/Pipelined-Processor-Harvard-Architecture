d1c44bda09e4999190e138a5aa611c6ee6d7c338
Mostafa Osama <31553087+Battarshuck@users.noreply.github.com>
2023-04-27 04:13:14 +0200

    assembler inshallah done

--- assembler.py
+++ assembler.py
@@ -19,7 +19,7 @@ opCodes: Dict = {
     "CALL": "10000",
     "RET": "10001",
     "RTI": "10010",
-    "LD": "10011",
+    "LDD": "10011",
     "STD": "10100",
     "JZ": "10101",
     "JC": "10110",
@@ -50,7 +50,7 @@ twoOperands: Set = {
     "INC",
     "DEC",
     "MOV",
-    "LD",
+    "LDD",
     "STD",
     "LDM"
 }
@@ -68,7 +68,7 @@ immediateOperands: Set = {
 }
 
 diffrentBehaviorOperands: Set = {
-    "LD",
+    "LDD",
     "STD"
 }
 
@@ -83,6 +83,14 @@ registers: Dict = {
     "R7": "111"
 }
 
+hexDigits: Set = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"}
+
+
+def ishex(s):
+    for c in s:
+        if not c in hexDigits:
+            return False
+    return True
 
 # this function get instruction string and returns all operands in this instrucion
 # In r1, r2 --> ['IN', 'R1', 'R2']
@@ -111,21 +119,22 @@ def expectedNumberOfOperands(instruction: str) -> int:
     if instruction in threeOperands:
         return 3
 
+def writeInFinaleFile(instructionCode: str, lineNumber: int, outputFile):
+    outputFile.write(str(lineNumber)+": " + instructionCode + "\n")
+    
 
-def compile(nameInputFile: str, nameOutputFIle: str):
-    outputFile = open(nameOutputFIle, "w")
+def compile(nameInputFile: str, outputFile):
     lineNumber: int = 0
     with open(nameInputFile, "r") as inputFile:
         for line in inputFile:
             instructionCode: str = ""
-            lineNumber += 1
             currentOperands: List = splitInstruction(line)
 
             if currentOperands[0] not in opCodes:
                 raise Exception(
                     "(COMMAND NOT FOUND) Syntax error at line number " + str(lineNumber))
 
-            instructionCode += opCodes[currentOperands[0]]
+            instructionCode += opCodes[currentOperands[0]] #opcode
 
             expectedNumOfOperands: int = expectedNumberOfOperands(
                 currentOperands[0])
@@ -134,7 +143,12 @@ def compile(nameInputFile: str, nameOutputFIle: str):
                 raise Exception(
                     "(CHECK NUM OF OPERANDS) Syntax error at line number " + str(lineNumber))
 
+            # zero operands
+            if expectedNumOfOperands == 0:
+                instructionCode += "000000000000000000"
+                instructionCode = instructionCode[0:16]
 
+            # one operand
             if expectedNumOfOperands == 1:
                 if currentOperands[1] not in registers:
                     raise Exception(
@@ -143,7 +157,7 @@ def compile(nameInputFile: str, nameOutputFIle: str):
                 instructionCode += "000000000000000000"
                 instructionCode = instructionCode[0:16]
 
-
+            # two operands
             if expectedNumOfOperands == 2:
                 if currentOperands[0] not in diffrentBehaviorOperands and currentOperands[0] not in immediateOperands:
                     if currentOperands[1] not in registers or  currentOperands[2] not in registers:
@@ -152,8 +166,9 @@ def compile(nameInputFile: str, nameOutputFIle: str):
                     instructionCode += registers[currentOperands[1]]
                     instructionCode += registers[currentOperands[2]]
                     instructionCode += "000000000000000000"
-                    instructionCode = instructionCode[0:16]
+                    instructionCode = instructionCode[0:16]            
 
+            # diffrent behavior operands and num of operands is 3
             if expectedNumOfOperands == 3:
                 if currentOperands[0] not in immediateOperands:
                     if currentOperands[1] not in registers or  currentOperands[2] not in registers or  currentOperands[3] not in registers:
@@ -164,12 +179,68 @@ def compile(nameInputFile: str, nameOutputFIle: str):
                     instructionCode += registers[currentOperands[3]]
                     instructionCode += "000000000000000000"
                     instructionCode = instructionCode[0:16]
+
+
+            if currentOperands[0] in diffrentBehaviorOperands:
+                if currentOperands[1] not in registers or  currentOperands[2] not in registers:
+                    raise Exception(
+                        "(REG NAME NOT FOUND) Syntax error at line number " + str(lineNumber))
+                
+                if currentOperands[0] == "LDD":
+                    instructionCode += registers[currentOperands[1]]
+                    instructionCode += "000"
+                    instructionCode += registers[currentOperands[2]]
+                elif currentOperands[0] == "STD":
+                    instructionCode += "000"
+                    instructionCode += registers[currentOperands[2]]
+                    instructionCode += registers[currentOperands[1]]
+
+                instructionCode += "000000000000000000"
+                instructionCode = instructionCode[0:16]
+
+
+            # immediate operands
+            if currentOperands[0] in immediateOperands :
+                if currentOperands[1] not in registers:
+                    raise Exception(
+                        "(REG NAME NOT FOUND) Syntax error at line number " + str(lineNumber))
+                instructionCode += registers[currentOperands[1]] #Rdst
+
+                if expectedNumOfOperands == 3:
+                    instructionCode += registers[currentOperands[2]] #Rsrc1
+                
+                instructionCode += "000000000000000000"
+                instructionCode = instructionCode[0:15]
+                instructionCode += "1" 
+
+                immediateValue = currentOperands[2] if expectedNumOfOperands == 2 else currentOperands[3]
+                if len(immediateValue) > 4 or not ishex(immediateValue):
+                    raise Exception(
+                        "(WRONG IMMEDIATE VALUE) Syntax error at line number " + str(lineNumber)) 
+
+                immediateValue = bin(int(immediateValue,16))[2:].zfill(16)
+
+
+            writeInFinaleFile(instructionCode, lineNumber, outputFile)
+            if currentOperands[0] in immediateOperands:
+                lineNumber+=1
+                writeInFinaleFile(immediateValue, lineNumber, outputFile)
+
+            lineNumber += 1
+                
+
             
 
-    outputFile.close()
+   
 
+outputFile = open("outputFile.mem", "w")
+outputFile.write("// memory data file (do not edit the following line - required for mem load use)\n")
+outputFile.write("// instance=/processor/fetchStage/instructionCache/ram\n")
+outputFile.write("// format=mti addressradix=d dataradix=b version=1.0 wordsperline=1\n")
 
 try:
-    compile("inputFile.txt", "outputFile.txt")
+    compile("inputFile.txt", outputFile)
 except Exception as e:
     print(e)
+
+outputFile.close()
